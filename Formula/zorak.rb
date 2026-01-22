c13e7beccf75abca4e6050ef02fbb8ca84ff85a1
rohit19872 <rohitkumar.singh@eka.care>
2026-01-22 18:36:31 +0530

    rb refactor

--- Formula/zorak.rb
+++ Formula/zorak.rb
@@ -4,29 +4,16 @@ class Zorak < Formula
   version "0.1.0"
 
   on_macos do
-    if Hardware::CPU.arm?
       url "https://github.com/voidrohit/zorak/releases/download/v0.0.1/zorak-Darwin-arm64.tar.gz"
       sha256 "444ac4a2c8eb48fbee3838a5587d67dfaa66f48f61f81d5694052261cae1cb2a"
-    else
-      url "https://github.com/voidrohit/zorak/releases/download/v0.1.0/zorak-Darwin-x86_64.tar.gz"
-      sha256 "REPLACE_WITH_ACTUAL_SHA256"
-    end
   end
 
-  on_linux do
-    if Hardware::CPU.arm?
-      url "https://github.com/voidrohit/zorak/releases/download/v0.1.0/zorak-Linux-aarch64.tar.gz"
-      sha256 "REPLACE_WITH_ACTUAL_SHA256"
-    else
-      url "https://github.com/voidrohit/zorak/releases/download/v0.1.0/zorak-Linux-x86_64.tar.gz"
-      sha256 "REPLACE_WITH_ACTUAL_SHA256"
-    end
-  end
 
   def install
-    bin.install "zorak/bin/zorak"
-    bin.install "zorak/bin/zorak-k8s"
-  end
+    cd "zorak" do
+      bin.install "bin/zorak"
+      bin.install "bin/zorak-k8s"
+    end
 
   test do
     system "#{bin}/zorak", "--version"
