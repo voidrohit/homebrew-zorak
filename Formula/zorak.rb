af21d43c31827b365a2e1cb4694c24fb360408a5
rohit19872 <rohitkumar.singh@eka.care>
2026-01-22 18:37:46 +0530

    rb refactor v2

--- Formula/zorak.rb
+++ Formula/zorak.rb
@@ -4,16 +4,18 @@ class Zorak < Formula
   version "0.1.0"
 
   on_macos do
-      url "https://github.com/voidrohit/zorak/releases/download/v0.0.1/zorak-Darwin-arm64.tar.gz"
-      sha256 "444ac4a2c8eb48fbee3838a5587d67dfaa66f48f61f81d5694052261cae1cb2a"
-  end
+    depends_on arch: :arm64
 
+    url "https://github.com/voidrohit/zorak/releases/download/v0.0.1/zorak-Darwin-arm64.tar.gz"
+    sha256 "444ac4a2c8eb48fbee3838a5587d67dfaa66f48f61f81d5694052261cae1cb2a"
+  end
 
   def install
     cd "zorak" do
       bin.install "bin/zorak"
       bin.install "bin/zorak-k8s"
     end
+  end
 
   test do
     system "#{bin}/zorak", "--version"
